604bbc6d2a10d45b963a8ccf09cc3e06c9036dce
Glitch (gustheitguru-fcc-temp-engine) <none>
2020-06-09 00:25:14 +0000

    🏢✊ Updated with Glitch

--- server.js
+++ server.js
@@ -4,15 +4,19 @@ const express = require("express");
 const fccTesting = require("./freeCodeCamp/fcctesting.js");
 
 const app = express();
+var pug = require('pug');
 
 fccTesting(app); //For FCC testing purposes
 app.use("/public", express.static(process.cwd() + "/public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.route("/").get((req, res) => {
-  //Change the response to render the Pug template
-  res.send(`Pug template is not defined.`);
+app.set('view engine','pug'); //set view engine so pug files will change over to html
+app.route('/') // home page index Q1
+.get((req, res) => { //get request function Q2
+  res.render(process.cwd() + '/views/pug/index', {title: 'Hello', message: 'Please login'}); 
+  // res.render (pug command)
+  // process.cwd 
 });
 
 app.listen(process.env.PORT || 3000, () => {
