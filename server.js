39d718e6ecb9225e342ea8da94d047c1f827d4c4
Glitch (gustheitguru-fcc-temp-engine) <none>
2020-06-09 00:40:42 +0000

    🐽🚶 Updated with Glitch

--- server.js
+++ server.js
@@ -2,6 +2,8 @@
 
 const express = require("express");
 const fccTesting = require("./freeCodeCamp/fcctesting.js");
+const session = require("express-session");
+const passport = require("passport");
 
 const app = express();
 var pug = require('pug');
@@ -11,13 +13,23 @@ app.use("/public", express.static(process.cwd() + "/public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+//Q3 --------------------------
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+//-------------------------------
+
 app.set('view engine','pug'); //set view engine so pug files will change over to html
 app.route('/') // home page index Q1
 .get((req, res) => { //get request function Q2
   res.render(process.cwd() + '/views/pug/index', {title: 'Hello', message: 'Please login'}); 
-  // res.render (pug command)
-  // process.cwd 
+  // field give on Q2
 });
+
 
 app.listen(process.env.PORT || 3000, () => {
   console.log("Listening on port " + process.env.PORT);
