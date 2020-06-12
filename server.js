7ff68d5d2e93617ee5959b1cd341f3c68892c39e
Glitch (gustheitguru-fcc-temp-engine) <none>
2020-06-12 03:32:35 +0000

    🌟🎌 Updated with Glitch

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const passport = require("passport");
 const ObjectID = require("mongodb").ObjectID;
 const mongo = require("mongodb").MongoClient;
 const env = require("dotenv").config();
+const LocalStrategy = require("passport-local");
 
 const app = express();
 var pug = require("pug");
@@ -15,6 +16,18 @@ fccTesting(app); //For FCC testing purposes
 app.use("/public", express.static(process.cwd() + "/public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.set("view engine", "pug"); //set view engine so pug files will change over to html
+app
+  .route("/") // home page index Q1
+  .get((req, res) => {
+    //get request function Q2
+    res.render(process.cwd() + "/views/pug/index", {
+      title: "Hello",
+      message: "Please login"
+    });
+    // field give on Q2
+  });
 
 //Q3 --------------------------
 app.use(
@@ -34,29 +47,74 @@ mongo.connect(process.env.DATABASE, { useUnifiedTopology: true }, (err, db) => {
   } else {
     console.log("Successful database connection");
   }
+  app.listen();
 
   passport.serializeUser((user, done) => {
     done(null, user._id);
   });
-  
+
   passport.deserializeUser((id, done) => {
     db.collection("users").findOne({ _id: new ObjectID(id) }, (err, doc) => {
       done(null, doc);
     });
   });
+  //Q6-----------------------
+  passport.use(
+    new LocalStrategy(function(username, password, done) {
+      db.collection("users").findOne({ username: username }, function(
+        err,
+        user
+      ) {
+        console.log("User " + username + " attempted to log in.");
+        if (err) {
+          return done(err);
+        }
+        if (!user) {
+          return done(null, false);
+        }
+        if (password !== user.password) {
+          return done(null, false);
+        }
+        return done(null, user);
+      });
+    })
+  );
+  //-------------------------
 });
 
-app.set("view engine", "pug"); //set view engine so pug files will change over to html
-app
-  .route("/") // home page index Q1
-  .get((req, res) => {
-    //get request function Q2
-    res.render(process.cwd() + "/views/pug/index", {
-      title: "Hello",
-      message: "Please login"
-    });
-    // field give on Q2
+//Q7-------------------------
+app.route("/").get((req, res) => {
+  res.render(process.cwd() + "/views/pug/index", {
+    title: "Hello",
+    message: "login",
+    showLogin: true
   });
+});
+
+app
+  .route("/login")
+  .post(
+    passport.authenticate("local", { failureRedirect: "/" }),
+    (req, res) => {
+      res.redirect("/profile");
+    }
+  );
+//Q8------------------------
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect('/');
+};
+
+ app
+ .route('/profile')
+ .get(ensureAuthenticated, (req,res) => {
+    res.render(process.cwd() + '/views/pug/profile',);
+ });
+
+//--------------------------
+//--------------------------
 
 app.listen(process.env.PORT || 3000, () => {
   console.log("Listening on port " + process.env.PORT);
